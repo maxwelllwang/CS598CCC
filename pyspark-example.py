ec6440f52bf206a63417e47dd19074bf9d20e108
maxwelllwang <maxwelllwang@gmail.com>
2023-12-02 08:37:12 +0000

    consumer working

--- pyspark-example.py
+++ pyspark-example.py
@@ -1,8 +1,10 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, udf
-from pyspark.sql.types import StringType, IntegerType, BooleanType,FloatType, StructType, StructField, DoubleType
+from pyspark.sql.functions import col, from_json, udf,current_timestamp, unix_timestamp
+from pyspark.sql.types import StringType, IntegerType, BooleanType,FloatType, StructType, StructField, DoubleType, TimestampType
 import time 
 import random
+from datetime import datetime
+
 
 
 scala_version = '2.12'
@@ -19,6 +21,8 @@ spark = SparkSession.builder \
     .config("spark.jars.packages", ",".join(packages))\
     .getOrCreate()
 
+
+
 # Define Kafka parameters
 kafka_topic_name = "my-topic"
 kafka_bootstrap_servers = "localhost:9092"  # e.g., "localhost:9092"
@@ -31,7 +35,9 @@ df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
     .option("subscribe", nyc_taxi_topic_name) \
-    .load()
+    .load() \
+    .selectExpr("CAST(value AS STRING)", "timestamp")
+
 
 schema = StructType([
     StructField("time_ns", DoubleType(), True),
@@ -44,7 +50,12 @@ schema = StructType([
     StructField("sparkpi", IntegerType(), True),
 ])
 
-json_df = df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
+
+json_df = df.select(from_json(col("value"), schema).alias("data"), col("timestamp")) \
+    .select("data.*", "timestamp")
+
+
+json_df = json_df.withColumn("sparkpi", col("sparkpi").cast("integer"))
 
 
 def estimate_pi(num_samples):
@@ -55,7 +66,14 @@ def estimate_pi(num_samples):
         distance = x**2 + y**2
         if distance <= 1:
             inside_circle += 1
+
     return 4 * inside_circle / num_samples
+
+def calculate_latency(create, finish):
+
+
+    difference_in_ms = (finish - create).total_seconds() * 1000
+    return difference_in_ms
 
 def calculate_average_speed_mph(start_time_sec, end_time_sec, distance):
     time_diff_hours =((end_time_sec - start_time_sec)/ 360)
@@ -63,22 +81,19 @@ def calculate_average_speed_mph(start_time_sec, end_time_sec, distance):
         return 0
     return  distance / time_diff_hours
 
-def calculate_latency(submit_time_ns):
-    return (time.time_ns() - submit_time_ns)  / 1000000
 
-
-estimate_pi_udf = udf(estimate_pi, IntegerType())
-
+estimate_pi_udf = udf(estimate_pi, FloatType())
+calculate_latency_udf = udf(calculate_latency, FloatType())
 average_speed_udf = udf(calculate_average_speed_mph, FloatType())
 
-calculate_latency_udf = udf(calculate_latency, IntegerType())
-
-
-df_with_udfs = json_df.withColumn("latency", calculate_latency_udf(col("time_ns"))).withColumn("average_speed", average_speed_udf(col("tpep_pickup_datetime"), col("tpep_dropoff_datetime"), col("trip_distance"))).withColumn("pi_estimate", estimate_pi_udf(col("sparkpi")))
+df = json_df.withColumn("current_timestamp", current_timestamp())
+df = df.withColumn("pi_estimate", estimate_pi_udf(col('sparkpi')))
+df = df.withColumn("latency_ms", calculate_latency_udf(col("timestamp"), current_timestamp()))
+df = df.withColumn("average_speed", average_speed_udf(col("tpep_pickup_datetime"), col("tpep_dropoff_datetime"), col("trip_distance")))
 
 
 # Print to console
-query = df_with_udfs.writeStream \
+query = df.writeStream \
     .outputMode("append") \
     .format("console") \
     .start()
